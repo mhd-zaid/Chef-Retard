e5c02ca8332899cda60645c256ce8b2929c5677a
Zaid MOUHAMAD <zaidmouhamad@gmail.com>
2023-11-20 00:12:42 +0100

    add content to home

--- apps/landing/components/landing/Hero.vue
+++ apps/landing/components/landing/Hero.vue
@@ -17,4 +17,22 @@ const appConfig = useAppConfig()
       </div>
     </div>
   </div>
+  <main class="mx-auto max-w-4xl">
+    <article class="text-justify">
+      <h2>La Magie du Chef Retard à Paris : Une Odyssée Culinaire qui Vaut l'Attente</h2>
+
+      <p>Dans le cœur vibrant de Paris, un soir étoilé, les convives se sont rassemblés avec des attentes élevées dans un restaurant réputé. Le chef, reconnu pour son talent culinaire exceptionnel, était le maître incontesté de la cuisine raffinée. Cependant, ce soir-là, le temps semblait jouer un tour au chef renommé.</p>
+
+      <p>Alors que les convives s'impatientaient, les murmures inquiets se mêlaient à l'excitation palpable. Les minutes s'étiraient, mais la promesse d'une expérience culinaire unique maintenait l'enthousiasme à flot. Ce n'était pas un simple retard, c'était la délibération minutieuse d'un créateur, la perfection prenant son temps.</p>
+
+      <p>Pendant cette attente, quelque chose de magique s'est produit. Les convives, au lieu de la frustration habituelle, ont décidé de transformer ces instants en une célébration de la patience et de l'anticipation. Les conversations se sont transformées en éloges pour le chef absent, chacun partageant des expériences précédentes qui valaient bien quelques minutes de plus.</p>
+
+      <p>Quand le chef est finalement apparu, son sourire confiant a dissipé toute trace de mécontentement. Il a partagé une brève explication, un délai nécessaire pour obtenir des ingrédients frais et de qualité supérieure. Et puis, il a commencé son spectacle culinaire.</p>
+
+      <p>Les plats qui ont suivi étaient des œuvres d'art comestibles. Chaque saveur était une harmonie calculée, chaque plat une symphonie de goûts. Les convives ont réalisé que la patience était la clé d'une expérience culinaire inoubliable.</p>
+
+      <p>C'est ainsi que notre site, dédié au talent unique du chef même en cas de retard, a été inspiré. Parce que parfois, la perfection nécessite du temps. Nous vous invitons à rejoindre notre aventure, où chaque instant d'attente est une promesse de délice. Découvrez nos conseils d'expert pour transformer chaque repas en une expérience culinaire exceptionnelle à Paris et au-delà. <a href="./conseils">Explorez nos conseils d'expert</a>.</p>
+    </article>
+
+  </main>
 </template>
