a324f762203fdf5cb65b4f8bd85c3b87eab67e10
Zaid MOUHAMAD <zaidmouhamad@gmail.com>
2023-11-20 01:13:35 +0100

    contact content

--- apps/contact/pages/contact/index.vue
+++ apps/contact/pages/contact/index.vue
@@ -9,5 +9,6 @@ useHead({
     <h1 class="text-3xl mb-3 mt-2 font-semibold tracking-normal text-gray-800">
       Contact
     </h1>
+    <p>Contactez nous sur contact.chefontime.tech pour plus de détails sur nos conseils chef retard
   </div>
 </template>
