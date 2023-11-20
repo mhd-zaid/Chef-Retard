6356c1edb26079695c229d97a6292aa334a8eeb5
Zaid MOUHAMAD <zaidmouhamad@gmail.com>
2023-11-20 01:14:46 +0100

    add end tag

--- apps/contact/pages/contact/index.vue
+++ apps/contact/pages/contact/index.vue
@@ -9,6 +9,6 @@ useHead({
     <h1 class="text-3xl mb-3 mt-2 font-semibold tracking-normal text-gray-800">
       Contact
     </h1>
-    <p>Contactez nous sur contact.chefontime.tech pour plus de détails sur nos conseils chef retard
+    <p>Contactez nous sur contact.chefontime.tech pour plus de détails sur nos conseils chef retard</p>
   </div>
 </template>
